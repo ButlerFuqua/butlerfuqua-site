e25bc06d1d28cdcebd82c2ac8ebe5d86a22589e0
Butler Fuqua <butlerfuqua@Butlers-MacBook-Pro-2.local>
2022-07-12 12:49:46 -0400

    Under construction

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Butler Fuqua</title>
+	<meta name="description" content="Personal Website for Butler Fuqua" />
+</svelte:head>
+
+<section>
+	<h1>Butler Fuqua</h1>
+	<h2>This website is under construction</h2>
+</section>
+
+<style>
+	section {
+		text-align: center;
+	}
+</style>
